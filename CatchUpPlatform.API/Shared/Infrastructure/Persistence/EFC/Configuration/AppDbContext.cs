a329b114ec98cd3e8cd3dce28b564be711c699de
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-24 18:24:22 -0500

    feat(database): add favorite source entity configuration to app db context.

--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,4 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
 using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +16,16 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // News Bounded Context Object-Relational Mapping Configurations
+        builder.Entity<FavoriteSource>().HasKey(f => f.Id);
+        builder.Entity<FavoriteSource>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<FavoriteSource>().Property(f => f.NewsApiKey).IsRequired().HasMaxLength(100);
+        builder.Entity<FavoriteSource>().Property(f => f.SourceId).IsRequired().HasMaxLength(100);
+        builder.Entity<FavoriteSource>().HasIndex(f => new { f.NewsApiKey, f.SourceId }).IsUnique();
+        
+        
+        
         
         // Apply snake_case naming convention to database objects
         builder.UseSnakeCaseNamingConvention();
