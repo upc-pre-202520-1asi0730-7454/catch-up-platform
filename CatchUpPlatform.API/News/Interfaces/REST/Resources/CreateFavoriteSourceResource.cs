507e91430ed918677c8dfd95644c3aa20d59fc13
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-24 17:05:07 -0500

    feat(favorite-source): add resource models for creating and representing favorite sources.

---
+++ CatchUpPlatform.API/News/Interfaces/REST/Resources/CreateFavoriteSourceResource.cs
@@ -0,0 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST.Resources;
+
+public record CreateFavoriteSourceResource(
+    [Required] string NewsApiKey,
+    [Required] string SourceId);
